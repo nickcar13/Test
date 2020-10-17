17a90bcd0b1ff3904584467fc3adbad86b3129a0
Nico Castañeda <nickcar13@gmail.com>
2020-10-17 16:23:43 -0500

    Updates

---
+++ precache-manifest.883f97f02102bc0228de2ea6ea0ff6ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da69c368e89f55f098ef5121ef47470e",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "055a6d7f5b8469715973",
+    "url": "/Test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84e784333fef1fbf7078",
+    "url": "/Test/static/js/2.012f29b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Test/static/js/2.012f29b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055a6d7f5b8469715973",
+    "url": "/Test/static/js/main.727ea0df.chunk.js"
+  },
+  {
+    "revision": "d9f4fa90447b19528f52",
+    "url": "/Test/static/js/runtime-main.d9cec00d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test/static/media/logo.5d5d9eef.svg"
+  }
+]);
